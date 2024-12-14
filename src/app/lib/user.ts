3fca79b1eba105086676ef48663fa3fdb3c7726d
DemoN <demon.ua.zp@gmail.com>
2024-12-14 19:52:30 +0200

    update

--- src/app/lib/user.ts
+++ src/app/lib/user.ts
@@ -1,4 +1,8 @@
-import { User } from '@prisma/client';
+import { Session, User } from '@prisma/client';
+import * as jose from 'jose';
+import { cookies } from 'next/headers';
+import { NextRequest, userAgent } from 'next/server';
+import { prisma } from '../../../prisma/prisma-client';
 
 export const userDataToOwner = (user: User) => {
     return {
@@ -11,4 +15,70 @@ export const userDataToOwner = (user: User) => {
     }
 }
 
-export const secretForJoes = ()=> new TextEncoder().encode(process.env.TOKEN_SALT as string);
+export const updateToken = async (uid: string, name: string, request: NextRequest, oldToken: string) => {
+    const token = await new jose.SignJWT({ uid, name })
+        .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+        .setIssuedAt()
+        .setExpirationTime('2m')
+        .sign(secretForJoes());
+
+    const { os, device, browser } = userAgent(request);
+
+    const agent = String(device.vendor) + ' '
+        + String(device.model) + ' '
+        + String(device.type) + ' '
+        + String(os.name) + ' '
+        + String(os.version) + ' '
+        + String(browser.name) + ' '
+        + String(browser.version);
+    console.log('agent = ', agent);
+    let userDbSession:Session|null;
+    try {
+        userDbSession = await prisma.session.findFirst({
+            where: {
+                token: oldToken,
+                agent
+            }
+        });
+    
+        if (!userDbSession) {
+            return Promise.reject('user not found!');
+        }
+    } catch (error) {
+        console.log(error);
+        return Promise.reject('some error!');
+    }
+    
+    console.log('userDbSession = ', userDbSession.userId);
+    const sessionDB = {
+        userId: userDbSession.userId,
+        agent,
+        token,
+        expiration: userDbSession.expiration
+    };
+
+    await prisma.session.update({
+        where: {
+            id: userDbSession.id
+        },
+        data: sessionDB
+    });
+    console.log('prisma.session.update = ');
+    const cookieStore = await cookies();
+    console.log('cookieStore');
+    //const stkey = cookieStore.get('stkey');
+    cookieStore.set('stkey', token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: userDbSession.expiration,
+        path: '/',
+    });
+
+    return 'success!';
+
+    //cookieStore.delete('stkey');
+};
+
+//export const secretForJoes = ()=> new TextEncoder().encode('dawd');
+
+export const secretForJoes = () => new TextEncoder().encode(process.env.TOKEN_SALT as string);
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,59 +2,99 @@ import { NextRequest, NextResponse } from 'next/server';
 //import { decrypt } from '@/app/lib/session'
 import { cookies } from 'next/headers';
 import * as jose from 'jose';
-import { secretForJoes } from './app/lib/user';
- 
+import { secretForJoes, updateToken } from './app/lib/user';
+import { JOSEError } from 'jose/errors';
+
 // 1. Specify protected and public routes
 const protectedRoutes = ['/dashboard'];
 const publicRoutes = [
-  '/login', 
-  '/signup', 
+  '/login',
+  '/signup',
   '/'
 ];
- 
+
 export default async function middleware(req: NextRequest) {
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   //const isPublicRoute = publicRoutes.includes(path);
- 
+
   // 3. Decrypt the session from the cookie
   const cookie = (await cookies()).get('stkey')?.value;
   console.log('cookie = ', cookie);
   //const session = await decrypt(cookie)
- 
+
   // 4. Redirect to /login if the user is not authenticated
   if (isProtectedRoute) {
     return NextResponse.redirect(new URL('/signin', req.nextUrl));
   }
- 
-  if((path==='/signin'||path==='/signup')&&cookie){
+
+  if ((path === '/signin' || path === '/signup') && cookie) {
     return NextResponse.redirect(new URL('/', req.nextUrl));
   }
 
-  if(cookie){
+  let response = NextResponse.next();
+
+  if (cookie) {
     //updateToken(cookie);
     try {
       //const salt:string = process.env.TOKEN_SALT as string;
 
       const decoded = await jose.jwtVerify(cookie, secretForJoes());
       console.log('decoded = ', decoded);
-    } catch(err) {
-      console.log('jwt err = ', err);
+    } catch (err) {
+      if (err instanceof jose.errors.JWTExpired) {
+        if (err.payload.hasOwnProperty('uid')&&err.payload.hasOwnProperty('name')) {
+          //console.log(err.payload);
+          try {
+            console.log('token start Update!');
+            await updateToken(err.payload.uid as string, err.payload.name as string, req, cookie);
+            console.log('token is Update!');
+          } catch (error) {
+            console.log('error update token must delete cookie');
+            response.cookies.delete('isAuth');
+            response.cookies.delete('stkey');
+          }
+          
+        }
+        // ...
+      } else {
+        console.log('must delete cookie');
+        response.cookies.delete('isAuth');
+        response.cookies.delete('stkey');
+      }
+      // if((err as JOSEError).code==='ERR_JWT_EXPIRED'){
+      //   if((err as JOSEError).cause){
+      //     //(err as JOSEError).cause.
+      //   }
+      //   console.log('message = ', (err as JOSEError).message);
+      //   console.log('name = ', (err as JOSEError).name);
+      //   console.log('stack = ', (err as JOSEError).stack);
+      //   console.log('cause = ', (err as JOSEError).cause);
+      // }else{
+      //   console.log('must delete cookie');
+      //   response.cookies.delete('isAuth');
+      //   response.cookies.delete('stkey');
+      // }
+      //console.log('jwt err = ', (err as JOSEError).code);
+      //console.log('message = ', (err as JOSEError).message);
+      //console.log('name = ', (err as JOSEError).name);
+      //console.log('stack = ', (err as JOSEError).stack);
+      //console.log('cause = ', (err as JOSEError).cause);
     }
   }
-  
-  let response = NextResponse.next();
-  if(cookie){
+
+
+  if (cookie) {
     response.cookies.set('isAuth', 'true');
-  }else{
+  } else {
     response.cookies.set('isAuth', 'false');
   }
-  
+
   //return NextResponse.next();
   return response;
 }
- 
+
 // Routes Middleware should not run on
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
